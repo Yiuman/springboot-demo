9f2e7be2c8736fd666cb3caea2e03e79e7a94019
yiumanKam <415481084@qq.com>
2018-02-27 18:17:44 +0800

    frist commit

---
+++ src/main/java/kam/yiuman/demo/controller/DemoController.java
@@ -0,0 +1,38 @@
+package kam.yiuman.demo.controller;
+
+import kam.yiuman.demo.pojo.User;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequestMapping("/")
+@RestController
+public class DemoController {
+
+    @RequestMapping("index")
+    public String index(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("host", "http://blog.didispace.com");
+        // return模板文件的名称，对应src/main/resources/templates/index.html
+        return "index";
+    }
+
+    @RequestMapping("maptest")
+    public Map<String,String> jsonTest(){
+        Map<String,String> map = new HashMap<String, String>();
+        map.put("sssss","sssss");
+        map.put("aaaaa","aaaaa");
+        return map;
+    }
+
+    @RequestMapping("usermap")
+    public Map<String,String> getUser(User user){
+        Map<String,String> map = new HashMap<String, String>();
+        map.put("user",user.toString());
+        return map;
+    }
+}
---
+++ src/main/java/kam/yiuman/demo/pojo/User.java
@@ -0,0 +1,158 @@
+package kam.yiuman.demo.pojo;
+
+public class User {
+
+    /**
+     * ID
+     */
+    private Long userId;
+
+    /**
+     * 用户编码
+     */
+    private String userUUID;
+
+    /**
+     * 用户名
+     */
+    private String userName;
+
+    /**
+     * 登录名
+     */
+    private String loginName;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 全拼
+     */
+    private String allSpell;
+
+    /**
+     * 简拼
+     */
+    private String simpleSpell;
+
+    /**
+     * 用户状态
+     */
+    private String state;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 电话
+     */
+    private String phone;
+
+    public User() {
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserUUID() {
+        return userUUID;
+    }
+
+    public void setUserUUID(String userUUID) {
+        this.userUUID = userUUID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAllSpell() {
+        return allSpell;
+    }
+
+    public void setAllSpell(String allSpell) {
+        this.allSpell = allSpell;
+    }
+
+    public String getSimpleSpell() {
+        return simpleSpell;
+    }
+
+    public void setSimpleSpell(String simpleSpell) {
+        this.simpleSpell = simpleSpell;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        return userId.equals(user.userId);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return userId.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return userName;
+    }
+}
